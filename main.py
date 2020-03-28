5c3e9fcee26f059a694fa3c00b0743668cb8776c
Tpat21 <41355131+Tpat21@users.noreply.github.com>
2020-03-28 17:58:17 -0400

    Preprocessing data and images

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello World")
+import numpy as np
+import os
+import cv2
+from keras_preprocessing.image import load_img
+import matplotlib.pyplot as plt
+
+
+def Dataset_loader(DIR):
+    IMG = []
+    for image in os.listdir(DIR):
+        PATH = os.path.join(DIR, image)
+        imgArray = np.array((cv2.imread(PATH, 0)))
+
+        IMG.append(np.array(imgArray))
+
+    return (IMG)
+
+
+benign_test = np.array(Dataset_loader("/Users/tpat/PycharmProjects/skin-cancer/data/test/benign"))
+malign_test = np.array(Dataset_loader("/Users/tpat/PycharmProjects/skin-cancer/data/test/malignant"))
+
+malign_train = np.array(Dataset_loader("/Users/tpat/PycharmProjects/skin-cancer/data/train/malignant"))
+benign_train = np.array(Dataset_loader("/Users/tpat/PycharmProjects/skin-cancer/data/train/benign"))
+
+
